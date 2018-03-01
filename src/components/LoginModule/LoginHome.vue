c98d38420365f29cab97791b1c4620177fb93e90
Nicolas Lamoureux <nick@inick.ca>
2018-03-01 13:02:11 -0500

    first

---
+++ src/components/LoginModule/LoginHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+      
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  
+  export default {
+  
+  }
+</script>
+<style>
+
+</style>
